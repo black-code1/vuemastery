184a38adc0590cd903ebead46aeb66803d23c948
black-code1 <legrandmahamat1@gmail.com>
2022-04-11 15:32:01 +0100

    event handling

--- main.js
+++ main.js
@@ -3,6 +3,28 @@ var app = new Vue({
   data: {
     product: "Socks",
     image: "./assets/vmSocks-green-onWhite.jpg",
-    inStock: false,
+    inStock: true,
+    details: ["80% cotton", "20% polyester", "Gender-neutral"],
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: "green",
+        variantImage: "./assets/vmSocks-green-onWhite.jpg",
+      },
+      {
+        variantId: 2235,
+        variantColor: "blue",
+        variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+      },
+    ],
+    cart: 0,
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1;
+    },
+    updateProduct(variantImage) {
+      this.image = variantImage;
+    },
   },
 });
